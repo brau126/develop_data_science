3ebaa041462b17b7be8a72a698b7dd226b0ebb18
Braulio CB <142444767+brau126@users.noreply.github.com>
2025-06-15 10:13:52 -0600

    Importacion Datos Gas_Fuels_Projects.R
    Ya esta añadida la forma de importacion de los datos, cada uno debe colocar su propia ruta y ya quedarán acomodades como por mes de cada año, en total tenemos 110 datos

--- Academic/Time_Series/Projects/Gas_Fuels_Projects.R
+++ Academic/Time_Series/Projects/Gas_Fuels_Projects.R
@@ -6,6 +6,16 @@
 #            _ _, Oswaldo
 # Fecha: 
 ###################################################################################
+########################## Librerias ##########################
+# datos
+library(dplyr)
+library(tidyverse)
+
+# importar
+library(readxl)
+
+# graficas
+library(ggplot2)
 
 ########################## Resumen y Abstract ##########################
 
@@ -17,7 +27,22 @@
 
 
 ########################## Metodologia ##########################
+########################## Importacion Datos ##########################
+# se importan los datos de un archivo de excel
+ruta_raw_data <- "Series_T/Volumenesdeventadeexpendioalpublico.xlsx" # porfavor aquí cada quien colocar la ruta donde está guardando el archivo
+raw_data <- read_excel(ruta_raw_data, sheet = "ventas_Regular")
 
+# modificar a un data frame utilizable
+df_regular_og <- raw_data
+df_regular_og <- df_regular_og[-(72112:72115),]
+df_regular_og <- df_regular_og[-(1:5),]
+colnames(df_regular_og) <- c("anio","mes","subproducto","entidad","municipio","volumen")
+df_regular_og$volumen <- as.numeric(df_regular_og$volumen)
+
+# sumar datos de acuerdo a su mes y años
+df_regular_mes <- df_regular_og %>%
+  group_by(anio, mes) %>%
+  summarise(volumen_mensual = sum(volumen, na.rm = TRUE))
 
 ########################## Analisis Empirico ##########################
 
