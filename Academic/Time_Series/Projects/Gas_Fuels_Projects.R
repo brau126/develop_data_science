49e299e00b6c2de0b3f2cace4322b611ae5679e3
Braulio CB <142444767+brau126@users.noreply.github.com>
2025-06-15 10:55:31 -0600

    Formate Fechas Gas_Fuels_Projects.R
    Se añaden nuevas columnas a la tabla og para poder generar la fecha numérica y tener un objeto de tipo ts. Igualmente para poder graficar con ggplot de forma continua

--- Academic/Time_Series/Projects/Gas_Fuels_Projects.R
+++ Academic/Time_Series/Projects/Gas_Fuels_Projects.R
@@ -10,6 +10,7 @@
 # datos
 library(dplyr)
 library(tidyverse)
+library(lubridate)
 
 # importar
 library(readxl)
@@ -20,7 +21,7 @@ library(ggplot2)
 ########################## Resumen y Abstract ##########################
 
 
-########################## Inroduccion ##########################
+########################## Introduccion ##########################
 
 
 ########################## Marco Teorico ##########################
@@ -29,7 +30,7 @@ library(ggplot2)
 ########################## Metodologia ##########################
 ########################## Importacion Datos ##########################
 # se importan los datos de un archivo de excel
-ruta_raw_data <- "Series_T/Volumenesdeventadeexpendioalpublico.xlsx" # porfavor aquí cada quien colocar la ruta donde está guardando el archivo
+ruta_raw_data <- "Volumenesdeventadeexpendioalpublico.xlsx" # porfavor aquí cada quien colocar la ruta donde está guardando el archivo
 raw_data <- read_excel(ruta_raw_data, sheet = "ventas_Regular")
 
 # modificar a un data frame utilizable
@@ -39,10 +40,20 @@ df_regular_og <- df_regular_og[-(1:5),]
 colnames(df_regular_og) <- c("anio","mes","subproducto","entidad","municipio","volumen")
 df_regular_og$volumen <- as.numeric(df_regular_og$volumen)
 
-# sumar datos de acuerdo a su mes y años
+# meses en espaniol
+meses <- c("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
+
+# coloca columna con el tiempo en forma numerica
+df_regular_og <- df_regular_og %>%
+  mutate(fecha = as.numeric(anio) + (match(mes, meses) / 12))
+
+# agrupa el volumen mensual sumando todos los municipios y estados
 df_regular_mes <- df_regular_og %>%
-  group_by(anio, mes) %>%
+  group_by(fecha) %>%
   summarise(volumen_mensual = sum(volumen, na.rm = TRUE))
+
+# se crea el objeto tipo time series
+ts_regular_mes <- ts(df_regular_mes$volumen_mensual, start = c(2016,1))
 
 ########################## Analisis Empirico ##########################
 
