4a570196d027fda58c5ba8340e2c40b282b256c1
Braulio CB <142444767+brau126@users.noreply.github.com>
2025-06-17 01:23:08 -0600

    Millones y AnEmpirico Gas_Fuels_Projects.R
    Se modifican la escala de los datos para facilitar procesamiento, interpretación de variables, medidas y al momento de la proyección; usando litros en millones. Igualmente se añaden gráficas para comprehender el comportamiento de los datos y determinar proximos pasos a tomar.

--- Academic/Time_Series/Projects/Gas_Fuels_Projects.R
+++ Academic/Time_Series/Projects/Gas_Fuels_Projects.R
@@ -18,6 +18,9 @@ library(readxl)
 # graficas
 library(ggplot2)
 
+# estadistica
+library(moments)
+
 ########################## Resumen y Abstract ##########################
 
 
@@ -43,20 +46,61 @@ df_regular_og$volumen <- as.numeric(df_regular_og$volumen)
 # meses en espaniol
 meses <- c("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
 
-# coloca columna con el tiempo en forma numerica
+# sumar datos de acuerdo a su mes y años
 df_regular_og <- df_regular_og %>%
   mutate(fecha = as.numeric(anio) + (match(mes, meses) / 12))
 
-# agrupa el volumen mensual sumando todos los municipios y estados
+# se genera el data frame y se usan millones para facilitar interpretacion y lectura
 df_regular_mes <- df_regular_og %>%
   group_by(fecha) %>%
-  summarise(volumen_mensual = sum(volumen, na.rm = TRUE))
+  summarise(volumen_mensual_mll = sum(volumen, na.rm = TRUE)) / 1000000
 
 # se crea el objeto tipo time series
-ts_regular_mes <- ts(df_regular_mes$volumen_mensual, start = c(2016,1))
+ts_regular_mes <- ts(df_regular_mes$volumen_mensual_mll, start = c(2016,1), frequency = 12)
+
+# configuracion anti notación cinetifica
+options(scipen = 999)
+
+# visualizacion del correlograma
+acf(ts_regular_mes, col = "palegreen4")
 
 ########################## Analisis Empirico ##########################
+# tabla de estadisticos descriptivos
+descripcion <- data.frame(
+  Media = mean(df_regular_mes$volumen_mensual_mll),
+  Mediana = median(df_regular_mes$volumen_mensual_mll),
+  Min = min(df_regular_mes$volumen_mensual_mll),
+  Max = max(df_regular_mes$volumen_mensual_mll),
+  Varianza = var(df_regular_mes$volumen_mensual_mll),
+  Sesgo = skewness(df_regular_mes$volumen_mensual_mll),
+  Curtosis = kurtosis(df_regular_mes$volumen_mensual_mll)
+  )
 
+descripcion
+
+# primera visualizacion de los datos
+ggplot(df_regular_mes) +
+  geom_line(mapping = aes(x = fecha, y = volumen_mensual_mll), color = "palegreen4") +
+  labs(title = "Volumen de Gas Regular Mensual ene 2016 - feb 2025", x = "Fecha", y = "Volumen en Millones") +
+  theme_minimal()
+
+# revisamos si con los valores
+df_regular_mes$volumen_indice <- df_regular_mes$volumen_mensual / mean(df_regular_mes$volumen_mensual)
+
+# graficamos el tras haber tranformado en indice
+ggplot(df_regular_mes) +
+  geom_line(mapping = aes(x = fecha, y = volumen_indice), color = "palegreen4") +
+  labs(title = "Indice de Volumen ene 2016 - feb 2025", x = "Fecha", y = "Indice del Volumen") +
+  theme_minimal()
+
+# observamos los ciclos anuales del volumen
+boxplot(ts_regular_mes ~ cycle(ts_regular_mes), main ="Boxplot Volumenes en Millones por Mes", xlab = "Mes Numérico", ylab = "Volumen en Millones", col = "palegreen2")
+
+# se realiza la descomposicion de los componentes de la serie temporal
+regular_mes_decomp = decompose(ts_regular_mes, type = "mult")
+
+# se genera la grafica de sus componentes
+plot(regular_mes_decomp, col = "palegreen4")
 
 ########################## Conclusiones ##########################
 
